9aa3e2c8bbb9ea01dc0915b19bbdf96dc9fb68cd
ezreal <956371950@qq.com>
2024-10-27 00:36:07 +0800

    project init

---
+++ payment-web/src/main/java/com/ezreal/small/payment/PaymentApplication.java
@@ -0,0 +1,15 @@
+package com.ezreal.small.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ezreal
+ * @Date 2024/10/27
+ */
+@SpringBootApplication
+public class PaymentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication.class, args);
+    }
+}
